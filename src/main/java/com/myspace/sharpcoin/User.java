6f575f2b572f8bf9b99803fabb698618a8ca1146
wbadmin <>
2019-06-03 20:20:47 +0200

    {/src/main/java/com/myspace/sharpcoin/User.java}

--- src/main/java/com/myspace/sharpcoin/User.java
+++ src/main/java/com/myspace/sharpcoin/User.java
@@ -8,7 +8,41 @@ public class User implements java.io.Serializable {
 
 	static final long serialVersionUID = 1L;
 
+	private java.lang.String name;
+	private java.lang.String email;
+	private int age;
+
 	public User() {
+	}
+
+	public java.lang.String getName() {
+		return this.name;
+	}
+
+	public void setName(java.lang.String name) {
+		this.name = name;
+	}
+
+	public java.lang.String getEmail() {
+		return this.email;
+	}
+
+	public void setEmail(java.lang.String email) {
+		this.email = email;
+	}
+
+	public int getAge() {
+		return this.age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public User(java.lang.String name, java.lang.String email, int age) {
+		this.name = name;
+		this.email = email;
+		this.age = age;
 	}
 
 }
